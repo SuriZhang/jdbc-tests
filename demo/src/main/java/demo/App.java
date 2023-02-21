ea03e7cb71801e09ae23ad42604314a3e117a8c0
SuriZhang <surizhang1224@gmail.com>
2023-02-21 13:42:52 +0800

    project setup, ConnectionTest passed on pgjdbc

---
+++ demo/src/main/java/demo/App.java
@@ -0,0 +1,14 @@
+package demo;
+import java.sql.Connection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
