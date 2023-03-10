d220a96bf809dd40b3412685e8402b1af09c81c2
SuriZhang <surizhang1224@gmail.com>
2023-03-10 16:03:34 +0800

    add new tests

--- demo/src/test/java/demo/test/StateMachineTest.java
+++ demo/src/test/java/demo/test/StateMachineTest.java
@@ -2,21 +2,39 @@ package demo.test;
 
 import org.junit.Test;
 
+import demo.GlobalInfo;
 import demo.state.Action;
 import demo.state.State;
-import demo.state.StateMachine;
+import demo.util.TestDbms;
 
 public class StateMachineTest {
     @Test
-    public void test() throws Exception {
-        StateMachine stateMachine = new StateMachine();
-        stateMachine.showCurrentState();
-        do {
-            Action a = stateMachine.action();
-            System.out.println("\t Perform Action: " + stateMachine.action());
-            a.invoke();
-            stateMachine.showCurrentState();
-        } while (stateMachine.getCurrentState() != State.FINAL && stateMachine.getCurrentState() != State.EXCEPTION);
+    public void test_1() throws Exception {
+        for (int i = 0; i < 100; i++) {
+
+            GlobalInfo.setTestDbms(TestDbms.POSTGRES);
+            GlobalInfo.stateMachine.showCurrentState();
+            while (GlobalInfo.stateMachine.getCurrentState() != State.FINAL) {
+                GlobalInfo.stateMachine.selectAction();
+                System.out.println("\t Perform Action: " + GlobalInfo.stateMachine.getCurrentAction());
+                GlobalInfo.stateMachine.getCurrentAction().invoke();
+                GlobalInfo.stateMachine.showCurrentState();
+            }
+        }
+    }
+
+    @Test
+    public void test_2() throws Exception {
+        GlobalInfo.setTestDbms(TestDbms.POSTGRES);
+        Action.OPEN_READ_ONLY_CONNECTION.invoke();
+    }
+
+    @Test
+    public void test_3() throws Exception {
+        GlobalInfo.setTestDbms(TestDbms.POSTGRES);
+        Action.OPEN_CONNECTION.invoke();
+        Action.CREATE_TABLE.invoke();
+        Action.CREATE_TABLE.invoke();
 
     }
 }
